c04410aa3096ecfe1528da43df2e76ada965ada0
sanvi-2014girl <goswamilipi82@gmail.com>
2026-08-09 12:53:32 +0530

    hw

---
+++ first.js
@@ -0,0 +1,4 @@
+name="Sanvi"
+age = 11
+skills = "Html and CSS"
+console.log(`Hi my name is ${name}, my age is ${age} and my skills are ${skills}`)
---
+++ hw.js
@@ -0,0 +1,11 @@
+name="Sanvi"
+age = 11
+pet = "Thor and Lilo"
+school = "St.Augustine's Day School,Barrackpore"
+favourite_color = "pink and white"
+country = "India"
+grade = 6
+aim_in_life = "doctor"
+idol = "Priyanka Chopra"
+kpop_idol_fav = "BTS and BLACKPINK"
+console.log(`Hi! I am ${name}.I am ${age} yaers old.I have two dogs named ${pet}.My school's name is ${school}.My favourite colors are ${favourite_color}.I come from ${country}.I read in grade ${grade}.My aim i life is to become a ${aim_in_life}.My idol is ${idol}.My one and only favourite kpop groups are ${kpop_idol_fav}.`)
